ca4bd4871741f9b1c9bfce5544832575fb3c48b8
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2022-05-31 11:17:47 -0700

    Enter world notification

---
+++ ProjectMicro/Assets/Scripts/UI/EnterLocationNotification.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnterLocationNotification : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI notificationText;
+
+    private float timer = 0;
+    private float showNotificationThreshold = 10;
+
+    void Start()
+    {
+        Hide();
+    }
+
+    void Update()
+    {
+        // Only update if in world
+        if (CurrentMapType.Type != MapType.World)
+        {
+            return;
+        }
+
+        // Don't add to timer if menu is open
+        if (UIModality.Instance.IsDialogueOpen ||
+            UIModality.Instance.IsEscapeMenuOpen ||
+            UIModality.Instance.IsInventoryOpen)
+        {
+            Hide();
+            return;
+        }
+
+        // Don't add to timer if player is inputing directions 
+        if (Input.GetAxisRaw("Vertical") != 0 ||
+            Input.GetAxisRaw("Horizontal") != 0)
+        {
+            Hide();
+            return;
+        }
+
+        timer += Time.deltaTime;
+        if (timer > showNotificationThreshold)
+        {
+            StartCoroutine(Show());
+            timer = 0;
+        }
+    }
+
+    private void Hide()
+    {
+        StopAllCoroutines();
+        notificationText.gameObject.SetActive(false);
+    }
+
+    private IEnumerator Show()
+    {
+        notificationText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+
+        Hide();
+    }
+}
